f9738fbc870785ff5e2e0c434d5c6fe78ec28201
Brett <doffing81@protonmail.com>
2021-01-26 14:20:18 -0700

    Twitter API update

--- twitoff/models.py
+++ twitoff/models.py
@@ -9,6 +9,7 @@ class User(DB.Model):  # User Table
     """Twitter Users corresponding to Tweets"""
     id = DB.Column(DB.BigInteger, primary_key=True)  # id column
     name = DB.Column(DB.String, nullable=False)  # name column
+    newest_tweet_id = DB.Column(DB.BigInteger) # tracks newest tweet
 
     def __repr__(self):
         return "<User: {}>".format(self.name)
@@ -18,6 +19,7 @@ class Tweet(DB.Model):
     """Tweets corresponding to Users"""
     id = DB.Column(DB.BigInteger, primary_key=True)  # id column
     text = DB.Column(DB.Unicode(300))  # tweet text column - allows for emojis
+    vect = DB.Column(DB.PickleType, nullable=False) # vectorized tweet
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey(
         "user.id"), nullable=False)  # user_id column (corresponding user)
     user = DB.relationship("User",  # creates user link between tweets
@@ -27,17 +29,20 @@ class Tweet(DB.Model):
         return "<Tweet: {}>".format(self.text)
 
 
-def insert_example_users():
-    nick = User(id=1, name="Nick")
-    elon = User(id=2, name="elonmusk")
-    tweet1 = Tweet(id=1, text="Aaaarrrgh, I'm a pirate!", user=nick)
-    tweet2 = Tweet(id=2, text="That's it, that's the tweet.", user=nick)
-    tweet3 = Tweet(id=3, text="It will come together eventually.", user=nick)
-    tweet4 = Tweet(id=4, text="I didn't even inhale.", user=elon)
-    tweet5 = Tweet(id=5, text="Space Force is Starlink's army.", user=elon)
-    tweet6 = Tweet(id=6, text="I'm flying to Mars tomorrow!!!", user=elon)
+# def insert_example_users():
+#     nick = User(id=1, name="Nick")
+#     elon = User(id=2, name="elonmusk")
+#     tweet1 = Tweet(id=1, text="Aaaarrrgh, I'm a pirate!", user=nick)
+#     tweet2 = Tweet(id=2, text="That's it, that's the tweet.", user=nick)
+#     tweet3 = Tweet(id=3, text="It will come together eventually.", user=nick)
+#     tweet4 = Tweet(id=4, text="I didn't even inhale.", user=elon)
+#     tweet5 = Tweet(id=5, text="Space Force is Starlink's army.", user=elon)
+#     tweet6 = Tweet(id=6, text="I'm flying to Mars tomorrow!!!", user=elon)
 
-    DB.session.add(nick)
-    DB.session.add(elon)
-    DB.session.add_all([tweet1, tweet2, tweet3, tweet4, tweet5, tweet6])
-    DB.session.commit()
+#     DB.session.add(nick)
+#     DB.session.add(elon)
+#     DB.session.add_all([tweet1, tweet2, tweet3, tweet4, tweet5, tweet6])
+#     DB.session.commit()
+
+    # Twitter
+    
---
+++ twitoff/twitter.py
@@ -0,0 +1,67 @@
+"""Retrieve tweets, embeddings, and put them in our Database"""
+from os import getenv
+import tweepy
+import spacy
+from .models import DB, Tweet, User
+
+
+TWITTER_API_KEY=getenv('TWITTER_API_KEY')
+TWITTER_API_KEY_SECRET=getenv('TWITTER_API_KEY_SECRET')
+
+TWITTER_AUTH = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_KEY_SECRET)
+
+TWITTER = tweepy.API(TWITTER_AUTH)
+
+# nlp = spacy.load('my_model') TODO
+
+def vectorize_tweet(tweet_text):
+    return nlp(tweet_text).embeddings
+
+def update_or_add_user(username):
+    try:
+        """Create user based on `username`"""
+        twitter_user = TWITTER.get_user(username)
+
+        # crazy syntax: 
+        # If they exist then update that user, if we get something back
+        # then instantiate a new user
+        db_user = (User.query.get(twitter_user.id)) or User(
+            id=twitter_user.id, name=username)
+
+        # Add the user to our database
+        DB.session.add(db_user)
+
+        tweets = twitter_user.timeline(
+            count=200, 
+            exclude_replies=True, 
+            include_rts=False, 
+            tweet_mode='Extended'
+        )  # a list of tweets from `username`
+
+        # empty tweets = false
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
+
+        for tweet in tweets:
+            # vectorize each tweet
+            vectorized_tweet = vectorize_tweet(tweet.full_text)
+            # Create tweet to add to DB
+            db_tweet = Tweet(id=tweet.id, text=tweet.text,
+                             vect=vectorized_tweet)
+            # Append each tweet from `username` to username.tweets
+            db_user.tweets.append(db_tweet)
+            # Add db_tweet to Tweet DB
+            DB.session.add(db_tweet)
+    except Exception as e:
+        print(f'Error processing {username}: {e}')
+        raise e
+    else:
+        # Commit
+        DB.session.commit()
+
+
+    DB.session.commit()
+
+
+def insert_example_users():
+    update_or_add_user("elonmusk")
