b6c8f65e473ad90b8ae03d261f3e4b3892a312aa
Brett <doffing81@protonmail.com>
2021-01-26 17:09:55 -0700

    Removed comments

--- twitoff/models.py
+++ twitoff/models.py
@@ -27,22 +27,4 @@ class Tweet(DB.Model):
 
     def __repr__(self):
         return "<Tweet: {}>".format(self.text)
-
-
-# def insert_example_users():
-#     nick = User(id=1, name="Nick")
-#     elon = User(id=2, name="elonmusk")
-#     tweet1 = Tweet(id=1, text="Aaaarrrgh, I'm a pirate!", user=nick)
-#     tweet2 = Tweet(id=2, text="That's it, that's the tweet.", user=nick)
-#     tweet3 = Tweet(id=3, text="It will come together eventually.", user=nick)
-#     tweet4 = Tweet(id=4, text="I didn't even inhale.", user=elon)
-#     tweet5 = Tweet(id=5, text="Space Force is Starlink's army.", user=elon)
-#     tweet6 = Tweet(id=6, text="I'm flying to Mars tomorrow!!!", user=elon)
-
-#     DB.session.add(nick)
-#     DB.session.add(elon)
-#     DB.session.add_all([tweet1, tweet2, tweet3, tweet4, tweet5, tweet6])
-#     DB.session.commit()
-
-    # Twitter
     
